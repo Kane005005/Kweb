5f7de65033aff2512e3f990b63938f1febd6725b
Kane005005 <traorekanz@gmail.com>
2025-09-25 09:53:44 +0000

    Update HomePage.js
    demenagement

--- k-web/src/pages/HomePage.js
+++ k-web/src/pages/HomePage.js
@@ -1,14 +1,16 @@
 import React from 'react';
-import HeroSection from '../components/HeroSection';
-import ServicesSection from '../components/ServicesSection';
-import WhyChooseUs from '../components/WhyChooseUs';
+import './HomePage.css'; // Créez ce fichier pour le style
 
 const HomePage = () => {
   return (
-    <div>
-      <HeroSection />
-      <ServicesSection />
-      <WhyChooseUs />
+    <div className="homepage-container">
+      <div className="redirection-card">
+        <h1>Allez sur Kweb</h1>
+        <p>Nous avons déménagé ! Pour découvrir notre nouveau site, cliquez sur le bouton ci-dessous :</p>
+        <a href="https://kanweb.pythonanywhere.com/" className="redirection-button">
+          Visitez notre nouveau site
+        </a>
+      </div>
     </div>
   );
 };
